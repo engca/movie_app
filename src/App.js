78ca8caeafac9ed880b0409e785b4c71e9c14fc1
engca <enca1988@naver.com>
2019-12-18 14:05:26 +0900

    #2.3 .map Recap

--- src/App.js
+++ src/App.js
@@ -11,26 +11,25 @@ function Food({ name, picture }) {
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "https://woochonfood.com/wp-content/uploads/2019/05/%EB%B0%B0%EC%B6%94%EA%B9%80%EC%B9%98-Napa-Cabbage-Kimchi.jpg"
   },
   {
+    id: 2,
     name: "Bibimbap",
     image:
       "http://recipe1.ezmember.co.kr/cache/recipe/2015/06/01/0467fec40dc5df750f026e7a87b4b1261.jpg"
   }
 ];
 
+function renderFood(dish) {
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello!!!</h1>
-      {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
-    </div>
-  );
+  return <div className="App">{foodILike.map(renderFood)}</div>;
 }
 
 export default App;
