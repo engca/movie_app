37f5a9279d7604e42cd56a5d54e2bf9860ed28df
engca <enca1988@naver.com>
2019-12-18 23:08:35 +0900

    #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {};
+  minus = () => {};
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
+
+/**
+ * #2 JSX & Props
 function Food({ name, picture, rating }) {
   return (
     <div>
@@ -48,5 +67,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
