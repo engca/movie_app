1eb2319daa56a9b42b7b39dd5b1986540efa59b5
engca <enca1988@naver.com>
2019-12-18 14:28:12 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
-      <h3>I like {name}</h3>
-      <img src={picture} />
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
     image:
-      "https://woochonfood.com/wp-content/uploads/2019/05/%EB%B0%B0%EC%B6%94%EA%B9%80%EC%B9%98-Napa-Cabbage-Kimchi.jpg"
+      "https://woochonfood.com/wp-content/uploads/2019/05/%EB%B0%B0%EC%B6%94%EA%B9%80%EC%B9%98-Napa-Cabbage-Kimchi.jpg",
+    rating: 5
   },
   {
     id: 2,
     name: "Bibimbap",
     image:
-      "http://recipe1.ezmember.co.kr/cache/recipe/2015/06/01/0467fec40dc5df750f026e7a87b4b1261.jpg"
+      "http://recipe1.ezmember.co.kr/cache/recipe/2015/06/01/0467fec40dc5df750f026e7a87b4b1261.jpg",
+    rating: 4.7
   }
 ];
 
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.image} />;
-}
-
 function App() {
-  return <div className="App">{foodILike.map(renderFood)}</div>;
+  return (
+    <div className="App">
+      {foodILike.map(dish => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
